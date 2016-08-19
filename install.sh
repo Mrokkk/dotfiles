e2e71df245ea8f427b6e6a5d1fbb6f3f5e50c99d
Maciek <mrokk@protonmail.com>
2016-08-20 00:18:21 +0200

    WIP: develop installation script

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-cp `find . -type f -maxdepth 1` $HOME/ 2>/dev/null
+set -e
+echo "Copying files..."
+for file in $(find . -type f -not -name "install.sh" -maxdepth 1 2>/dev/null); do
+    echo "  Copying $file..."
+    [[ ! -e ~/$file ]] && cp $file ~/$file
+    [[ $(diff ~/$file $file) ]] && echo -n "    Differences found in $file. Do you wish to overwrite? [y/N] " && read input
+done
+
